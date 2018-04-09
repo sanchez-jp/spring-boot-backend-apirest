aa99d72e580408bbb3a263333915c07ea742014d
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 12:41:41 +0200

    Implementado CRUD en el Service de clientes del back-end:
      * Se han implementado métodos CRUD para la inserción, borrado y
      búsqueda de clientes en la capa de negocio (services)

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -15,6 +15,18 @@ public class ClienteServiceImpl implements IClienteService {
     private IClienteDao clienteDao;
 
     /**
+     * Obtiene un cliente por su identificador
+     *
+     * @param id el identificador del cliente
+     * @return el cliente buscado o NULL si no lo encuentra
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return clienteDao.findById(id).orElse(null);
+    }
+
+    /**
      * Obtiene todos los clientes de la base de datos
      *
      * @return el listado de todos los clientes almacenados en la base de datos
@@ -23,5 +35,28 @@ public class ClienteServiceImpl implements IClienteService {
     @Transactional(readOnly = true)
     public List<Cliente> findAll() {
         return (List<Cliente>) clienteDao.findAll();
+    }
+
+    /**
+     * Guarda un cliente en la base de datos
+     *
+     * @param cliente el cliente a guardar
+     * @return el cliente guardado
+     */
+    @Override
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return clienteDao.save(cliente);
+    }
+
+    /**
+     * Elimina un cliente de la base de datos por su identificador
+     *
+     * @param id el identificador del cliente a borrar.
+     */
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        clienteDao.deleteById(id);
     }
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -7,9 +7,33 @@ import java.util.List;
 public interface IClienteService {
 
     /**
+     * Obtiene un cliente por su identificador
+     *
+     * @param id el identificador del cliente
+     * @return el cliente buscado
+     */
+    Cliente findById(Long id);
+
+    /**
      * Obtiene todos los clientes de la base de datos
      *
      * @return el listado de todos los clientes almacenados en la base de datos
      */
-    public List<Cliente> findAll();
+    List<Cliente> findAll();
+
+    /**
+     * Guarda un cliente en la base de datos
+     *
+     * @param cliente el cliente a guardar
+     * @return el cliente guardado
+     */
+    Cliente save(Cliente cliente);
+
+    /**
+     * Elimina un cliente de la base de datos por su identificador
+     *
+     * @param id el identificador del cliente a borrar.
+     */
+    void delete(Long id);
+
 }
