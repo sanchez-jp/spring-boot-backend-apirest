98eb4d416be4808bc262f87228cee0e267458790
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 10:41:41 +0200

    Datos de prueba:
      * Añadido archivo "import.sql" con datos de prueba para la tabla
      clientes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,25 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Juan Pedro', 'Sánchez', 'juanpedro.sanchez@preving.com', '2017/12/11');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('María', 'de la Fuente', 'delafmaria@gmail.com', '2018/05/01');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Leonardo', 'Benitez', 'leonardo.benitez@preving.com', '2016/03/21');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Antonio', 'Melitón', 'antonio.meliton@preving.com', '2017/12/11');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Jesús', 'Gonzalez', 'jesus.gonzalez@preving.com', '2018/05/01');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Nieves', 'Rodríguez', 'nieves.rodriguez@preving.com', '2016/03/21');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Víctor', 'Saavedra', 'vitosaavedra@gmail.com', '2017/12/11');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Daniel', 'Curiel', 'curieldaniel@gmail.com', '2018/05/01');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Fernando', 'Ceballos', 'fernandoceballos1989@gmail.com', '2016/03/21');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Antonio', 'Guijarro', 'guijarroguijarro@gmail.com', '2017/12/11');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Javier', 'Montesinos', 'javier.montesinos@preving.com', '2018/05/01');
+INSERT INTO clientes (nombre, apellido, email, create_at)
+VALUES ('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2016/03/21');
