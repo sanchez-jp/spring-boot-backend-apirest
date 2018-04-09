7acf6874f846d2f135bc18e66b23f3abb207c39e
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-09 11:51:45 +0200

    Configuración CORS en el servidor:
      * Añadida notación @CrossOrigin en el controlador de clientes.
      Esta notación permite solicitudes de origen cruzado en clases
      específicas de controlador (como es el caso) y/o métodos de
      controlador.

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/controllers/ClienteRestController.java
@@ -3,12 +3,15 @@ package com.bolsadeideas.springboot.backend.apirest.models.controllers;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
+// Permite solicitudes de origen cruzado en clases específicas de controlador y / o métodos de controlador.
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
